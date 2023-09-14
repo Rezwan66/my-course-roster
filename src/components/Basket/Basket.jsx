959f431db6082b40e4a23454dbadd72d891f2a5a
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-15 03:28:15 +0600

    click select button to add courses+ cannot select again- functionality added

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Basket = props => {
+const Basket = ({ selectedCourses }) => {
   return (
     <div className="bg-white rounded-md card-body space-y-4">
       <h3 className="text-lg font-bold text-[#2F80ED]">
@@ -8,13 +8,21 @@ const Basket = props => {
       </h3>
       <hr />
       <h2 className="text-xl font-bold">Course Name</h2>
-      <ol></ol>
+      <ol className="list-decimal px-5 text-[#1C1B1B99]">
+        {selectedCourses.map((item, idx) => (
+          <li key={idx}>{item.name}</li>
+        ))}
+      </ol>
       <hr />
-      <h4 className="text-base font-medium">Total Credit Hour : </h4>
+      <h4 className="text-base font-medium text-[#1C1B1BCC]">
+        Total Credit Hour :{' '}
+      </h4>
     </div>
   );
 };
 
-Basket.propTypes = {};
+Basket.propTypes = {
+  selectedCourses: PropTypes.array.isRequired,
+};
 
 export default Basket;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,24 +5,38 @@ import Basket from '../Basket/Basket';
 
 const Home = () => {
   const [courses, setCourses] = useState([]);
+  const [selectedCourses, setSelectedCourses] = useState([]);
   useEffect(() => {
     fetch('data.json')
       .then(res => res.json())
       .then(data => setCourses(data));
   }, []);
   //   console.log(courses);
+
+  const handleSelectCourse = card => {
+    const isClicked = selectedCourses.find(sel => sel.id === card.id);
+    if (isClicked) {
+      return alert('Cant select again');
+    }
+    setSelectedCourses([...selectedCourses, card]);
+  };
+
   return (
     <div>
       <div className="flex  gap-8">
         {/* cards container */}
         <div className="grid grid-cols-3 gap-6">
           {courses.map(card => (
-            <Card key={card.id} card={card}></Card>
+            <Card
+              key={card.id}
+              card={card}
+              handleSelectCourse={handleSelectCourse}
+            ></Card>
           ))}
         </div>
         {/* basket container */}
         <div className="min-w-[320px]">
-          <Basket></Basket>
+          <Basket selectedCourses={selectedCourses}></Basket>
         </div>
       </div>
     </div>
